42751a10a11da867d0ac2ca6f3d3b44e68805d21
Ken Youens-Clark <kyclark@gmail.com>
2023-10-25 08:24:22 -0700

    simpler version

--- 04_headr/src/main.rs
+++ 04_headr/src/main.rs
@@ -1,6 +1,119 @@
+use anyhow::Result;
+use clap::{Arg, Command};
+use std::fs::File;
+use std::io::{self, BufRead, BufReader, Read};
+
+#[derive(Debug)]
+pub struct Config {
+    files: Vec<String>,
+    lines: u64,
+    bytes: Option<u64>,
+}
+
 fn main() {
-    if let Err(e) = headr::get_args().and_then(headr::run) {
+    if let Err(e) = run(get_args()) {
         eprintln!("{e}");
         std::process::exit(1);
+    }
+}
+
+// --------------------------------------------------
+fn get_args() -> Config {
+    let matches = Command::new("headr")
+        .version("0.1.0")
+        .author("Ken Youens-Clark <kyclark@gmail.com>")
+        .about("Rust head")
+        .arg(
+            Arg::new("lines")
+                .short('n')
+                .long("lines")
+                .value_name("LINES")
+                .help("Number of lines")
+                .value_parser(clap::value_parser!(u64).range(1..))
+                .default_value("10"),
+        )
+        .arg(
+            Arg::new("bytes")
+                .short('c')
+                .long("bytes")
+                .value_name("BYTES")
+                .num_args(0..=1)
+                .conflicts_with("lines")
+                .value_parser(clap::value_parser!(u64).range(1..))
+                .help("Number of bytes"),
+        )
+        .arg(
+            Arg::new("files")
+                .value_name("FILE")
+                .help("Input file(s)")
+                .num_args(1..)
+                .default_value("-"),
+        )
+        .get_matches();
+
+    Config {
+        files: matches
+            .get_many("files")
+            .expect("file required")
+            .cloned()
+            .collect(),
+        lines: matches.get_one("lines").cloned().unwrap(),
+        bytes: matches.get_one("bytes").cloned(),
+    }
+}
+
+// --------------------------------------------------
+//fn run(config: Config) -> Result<()> {
+//    dbg!(config);
+//    Ok(())
+//}
+
+// --------------------------------------------------
+fn run(config: Config) -> Result<()> {
+    let num_files = config.files.len();
+
+    for (file_num, filename) in config.files.iter().enumerate() {
+        match open(filename) {
+            Err(err) => eprintln!("{filename}: {err}"),
+            Ok(mut file) => {
+                if num_files > 1 {
+                    println!(
+                        "{}==> {} <==",
+                        if file_num > 0 { "\n" } else { "" },
+                        &filename
+                    );
+                }
+
+                if let Some(num_bytes) = config.bytes {
+                    let mut handle = file.take(num_bytes);
+                    let mut buffer = vec![0; num_bytes as usize];
+                    let bytes_read = handle.read(&mut buffer)?;
+                    print!(
+                        "{}",
+                        String::from_utf8_lossy(&buffer[..bytes_read])
+                    );
+                } else {
+                    let mut line = String::new();
+                    for _ in 0..config.lines {
+                        let bytes = file.read_line(&mut line)?;
+                        if bytes == 0 {
+                            break;
+                        }
+                        print!("{line}");
+                        line.clear();
+                    }
+                }
+            }
+        }
+    }
+
+    Ok(())
+}
+
+// --------------------------------------------------
+fn open(filename: &str) -> Result<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
     }
 }
